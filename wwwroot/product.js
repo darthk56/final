11b0b004a2a03a246d2d7858a59842edaeea6b84
jgrissom <jgrissom91@gmail.com>
2024-04-21 16:34:47 -0500

    [8.09] Display selected product id, name, price in modal - Index.cshtml - add div to store selected product id (do not display) - product.js - display product name, price in modal

--- wwwroot/product.js
+++ wwwroot/product.js
@@ -14,14 +14,20 @@ document.getElementById('product_rows').addEventListener("click", (e) => {
   if (p.classList.contains('product')) {
     e.preventDefault()
     // console.log(p.dataset['id']);
+    document.getElementById('ProductId').innerHTML = p.dataset['id'];
+    document.getElementById('ProductName').innerHTML = p.dataset['name'];
+    document.getElementById('UnitPrice').innerHTML = Number(p.dataset['price']).toFixed(2);
+    display_total();
     const cart = new bootstrap.Modal('#cartModal', {}).show();
   }
 });
+const display_total = () => {
+  const total = parseInt(document.getElementById('Quantity').value) * Number(document.getElementById('UnitPrice').innerHTML);
+  document.getElementById('Total').innerHTML = numberWithCommas(total.toFixed(2));
+}
 // update total when cart quantity is changed
 document.getElementById('Quantity').addEventListener("change", (e) => {
-  // console.log(e.target.value);
-  const total = parseInt(e.target.value) * Number(document.getElementById('UnitPrice').innerHTML);
-  document.getElementById('Total').innerHTML = numberWithCommas(total.toFixed(2));
+  display_total();
 });
 // function to display commas in number
 const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -34,7 +40,7 @@ async function fetchProducts() {
   fetchedProducts.map(product => {
     const css = product.discontinued ? " discontinued" : "";
     product_rows += 
-      `<tr class="product${css}" data-id="${product.productId}">
+      `<tr class="product${css}" data-id="${product.productId}" data-name="${product.productName}" data-price="${product.unitPrice}">
         <td>${product.productName}</td>
         <td class="text-end">${product.unitPrice.toFixed(2)}</td>
         <td class="text-end">${product.unitsInStock}</td>
